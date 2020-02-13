b372d8bf759eecbb208c8c7ea1bf9c21a90d2d5c
alphonsomckenzie <ajm6684@psu.edu>
2020-02-13 01:09:28 -0500

    Initial commit

---
+++ app/src/main/java/psu/ajm6684/patientmonitoringsystem/createAccount/createAccount.java
@@ -0,0 +1,16 @@
+package psu.ajm6684.patientmonitoringsystem.createAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import psu.ajm6684.patientmonitoringsystem.R;
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
