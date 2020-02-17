fc3985999173f9132ef22cd5051484883cd3a401
alphonsomckenzie <ajm6684@psu.edu>
2020-02-17 09:29:37 -0500

    Init push

--- app/src/main/java/psu/ajm6684/patientmonitoringsystem/databaseHandler.java
+++ app/src/main/java/psu/ajm6684/patientmonitoringsystem/databaseHandler.java
@@ -1,14 +1,32 @@
 package psu.ajm6684.patientmonitoringsystem;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class databaseHandler extends{
+import androidx.annotation.Nullable;
+
+public class databaseHandler extends SQLiteOpenHelper {
+    public databaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
 //    super(context, DATABASE_NAME,null,1);
 //    SQLiteDatabase db = this.getWrit
 
     public boolean insertData(String hospital, String firstName, String lastName, String position, String department, String userName, String employeeID){
 
+
+        return true;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
