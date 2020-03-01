a1d2fde3a983a0b4fcfa0d2624804c74fc1f0a60
Logan <ayleids@gmail.com>
2020-03-01 10:48:58 -0500

    react button

--- website/js/Somescript.js
+++ website/js/Somescript.js
@@ -20,3 +20,14 @@ class Somescript extends React.Component {
     );
     }
 }
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+        // Read the comment ID from a data-* attribute.
+        const commentID = parseInt(domContainer.dataset.commentid, 10);
+        ReactDOM.render(
+            e(LikeButton, { commentID: commentID }),
+            domContainer
+        );
+    });
