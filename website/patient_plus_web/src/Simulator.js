1dfa9a766e4e8bae4101c8487c0316565fed3d97
seantodd114 <57458555+seantodd114@users.noreply.github.com>
2020-03-15 22:03:51 -0400

    Added Router
    Doesn't quite work just yet. Also added simulator page.

---
+++ website/patient_plus_web/src/Simulator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'react-router-dom';
+
+class Simulator extends React.Component {
+    render() {
+        return(
+            <div className="Simulator">
+                <body>hi</body>
+            </div>
+        );
+    }
+}
+
+export default Simulator;
