6fdfa487a3fec3aac094db92295618814b446e2d
seantodd114 <57458555+seantodd114@users.noreply.github.com>
2020-03-22 12:32:11 -0400

    Updated promo site profile
    For some reason WebStorm imported the website folder weird and didn't seem to think of it as a project. Problem may be at my end, but warning everyone just in case.

--- website/src/components/sean.js
+++ website/src/components/sean.js
@@ -14,7 +14,7 @@ class Sean extends Component {
                             alt="avatar"
                             style={{height: '250px'}}
                         />
-                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Computer Science major at Penn State Harrisburg, expected to graduate Fall 2020.</p>
 
                     </Cell>
                     <Cell col={6}>
@@ -26,31 +26,23 @@ class Sean extends Component {
                                 <ListItem>
                                     <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
                                         <i className="fa fa-phone-square" aria-hidden="true"/>
-                                        (123) 456-7890
-                                    </ListItemContent>
-                                </ListItem>
-
-                                <ListItem>
-                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                                        <i className="fa fa-fax" aria-hidden="true"/>
-                                        (123) 456-7890
+                                        (717) 713-0395
                                     </ListItemContent>
                                 </ListItem>
 
                                 <ListItem>
                                     <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
                                         <i className="fa fa-envelope" aria-hidden="true"/>
-                                        someone@example.com
+                                        seantodd114@gmail.com
                                     </ListItemContent>
                                 </ListItem>
 
                                 <ListItem>
-                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                                        <i className="fa fa-skype" aria-hidden="true"/>
-                                        MySkypeID
+                                    <ListItemContent style={{fontSize: '20px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-github" aria-hidden="true"/>
+                                        https://github.com/seantodd114
                                     </ListItemContent>
                                 </ListItem>
-
 
                             </List>
                         </div>
