c9833a2917ea718119f36090d06f3ee819a0d1a9
Alphonso74 <ajm6684@psu.edu>
2020-03-28 12:51:05 -0400

    fix Detail buttons component

--- DataSim/data-sim/src/components/patients/PatientDetails.js
+++ DataSim/data-sim/src/components/patients/PatientDetails.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {firestoreConnect} from "react-redux-firebase";
 import {compose} from 'redux'
 import {Redirect} from 'react-router-dom'
+import DetailButtons from "./DetailButtons";
 
 
 const PatientDetails = (props) => {
