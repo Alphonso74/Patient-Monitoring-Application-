089b519e67e592836475d8fe0cafe07fd7bcfd1e
11CTodd <59945448+11CTodd@users.noreply.github.com>
2020-03-29 22:50:24 -0400

    Update PatientDetails.js
    Enabled editing

--- DataSim/data-sim/src/components/patients/PatientDetails.js
+++ DataSim/data-sim/src/components/patients/PatientDetails.js
@@ -1,52 +1,134 @@
-import React from "react";
+import React, { Component } from "react";
 import {connect} from 'react-redux'
 import {firestoreConnect} from "react-redux-firebase";
 import {compose} from 'redux'
 import {Redirect} from 'react-router-dom'
-import DetailButtons from "./DetailButtons";
+//import DetailButtons from "./DetailButtons";
+import {updatePatient} from "../../store/actions/patientActions";
 
 
-const PatientDetails = (props) => {
-    const {patient, auth} = props;
-    if(!auth.uid) return <Redirect to='/signin'/>
+class PatientDetails extends Component {
+    state = {
+        id: this.props.match.params.id,
+        patientName: this.props.patient.patientName,
+        description: this.props.patient.description,
+        height: this.props.patient.height,
+        weight: this.props.patient.weight,
+        rHeartRate: this.props.patient.rHeartRate,
+        triageTag:  this.props.patient.triageTag,
+        bodyTempature: this.props.patient.bodyTempature,
+        medications: this.props.patient.medications,
+        surgicaHistory: this.props.patient.surgicaHistory,
+        activeNurse: this.props.patient.activeNurse,
+        standingOrder: this.props.patient.standingOrder
 
-    if(patient){
+    };
 
-        return(
-            <div className="container section project-details">
-                <div className="card z-depth-0">
-                    <div className="card-content">
-                        <span className="card-title center">{patient.patientName}</span>
-                        <p>Patient Description: {patient.description}</p>
-                        <p>Height: {patient.height}</p>
-                        <p>Weight: {patient.weight}</p>
-                        <p>Body Temperature: {patient.bodyTempature}</p>
-                        <p>Heart Rate: {patient.rHeartRate}</p>
-                        <p>Triage Tag: {patient.triageTag}</p>
-                        {/*<p><input type="button" className="" value="Standing Order" onClick={() => handleClick("Standing Order", props)}/> <input type="button" className="" value="Chart" onClick={() => handleClick("Chart", props)}/></p>*/}
-                        {/*<input type="button" className="" value="Assign Nurse" onClick={() => handleClick("Assign Nurse", props)}/>*/}
+    handleClick = (e) => {
+        switch(e.target.id){
+            case "name":
+                this.setState({patientName: prompt("Enter new name:", this.state.patientName)});
+                break;
 
-                        <DetailButtons patient={{patient}}/>
+            case "desc":
+                this.setState({description: prompt("Enter new description:", this.state.description)});
+                break;
 
+            case "height":
+                this.setState({height: prompt("Enter new height:", this.state.height)});
+                break;
 
-                    </div>
-                    <div className="card-action grey lighten-4 grey-text">
-                        {/*<div>Posted by The Net Ninja</div>*/}
-                        {/*<div>2nd September, 2am</div>*/}
+            case "weight":
+                this.setState({weight: parseInt(prompt("Enter new weight:", this.state.weight))});
+                break;
+
+            case "temp":
+                this.setState({bodyTempature: parseInt(prompt("Enter new body temperature:", this.state.bodyTempature))});
+                break;
+
+            case "hRate":
+                this.setState({rHeartRate: parseInt(prompt("Enter new resting heart rate:", this.state.rHeartRate))});
+                break;
+
+            case "tag":
+                this.setState({triageTag: prompt("Enter new triage tag:", this.state.triageTag)});
+                break;
+
+            case "order":
+                this.setState({standingOrder: prompt("Enter new doctor standing order:", this.state.standingOrder)});
+                break;
+
+            case "meds":
+                this.setState({medications: prompt("Enter patient's medications:", this.state.medications)});
+                break;
+
+            case "surgHist":
+                this.setState({surgicaHistory: prompt("Enter patient's surgical history:", this.state.surgicaHistory)});
+                break;
+
+            case "submit":
+                e.preventDefault();
+                this.props.history.push('/');
+
+                console.log(this.state);
+                this.props.updatePatient(this.state, this.state.id);
+                break;
+
+            default:
+        }
+    };
+
+    render() {
+        const {patient, auth} = this.props;
+        if (!auth.uid) return <Redirect to='/signin'/>;
+
+        if (patient) {
+
+            return (
+                <div className="container section project-details">
+                    <div className="card z-depth-0">
+                        <div className="card-content">
+                            <span className="card-title center">{patient.patientName}<input id="name" type="button" className='button' value='Edit' onClick={this.handleClick}/></span>
+                            <p>Patient Description: {patient.description}
+                                <input id="desc" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Height: {patient.height}
+                                <input id="height" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Weight: {patient.weight}
+                                <input id="weight" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Body Temperature: {patient.bodyTempature}
+                                <input id="temp" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Heart Rate: {patient.rHeartRate}
+                                <input id="hRate" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Triage Tag: {patient.triageTag}
+                                <input id="tag" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Standing Order: {patient.standingOrder}
+                                <input id="order" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Medications: {patient.medications}
+                                <input id="meds" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Surgical History: {patient.surgicaHistory}
+                                <input id="surgHist" type="button" className='button' value='Edit' onClick={this.handleClick}/></p>
+                            <p>Active Nurse: {patient.activeNurse}</p>
+                            <p><input id="submit" type="button" className="button" value="Submit" onClick={this.handleClick}/></p>
+                            {/* <DetailButtons patient={{patient}}/> */}
+                        </div>
+
+                        <div className="card-action grey lighten-4 grey-text">
+                            {/*<div>Posted by The Net Ninja</div>*/}
+                            {/*<div>2nd September, 2am</div>*/}
+                        </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
-    else {
+            )
+        } else {
 
 
-        return (
-            <div className="container center">
-            <p>Loading Patient Data one second....</p>
-            </div>
+            return (
+                <div className="container center">
+                    <p>Loading Patient Data. One second....</p>
+                </div>
 
-        )
+            )
+        }
     }
 };
 
@@ -60,11 +142,17 @@ const mapStateToProps = (state, ownProps) => {
         auth: state.firebase.auth
 
     }
-}
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updatePatient: (patient, id) => dispatch(updatePatient(patient, id))
+    }
+};
 
 export default compose(
-    connect(mapStateToProps),
+    connect(mapStateToProps, mapDispatchToProps),
     firestoreConnect([
-        {collection: 'patients2'}
+        {collection: 'patients'}
     ])
 )(PatientDetails)
