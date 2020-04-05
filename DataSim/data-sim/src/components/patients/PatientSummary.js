25289748e0944a3b72ae8b924a4f53ec8a08a6c6
11CTodd <59945448+11CTodd@users.noreply.github.com>
2020-04-05 16:33:06 -0400

    Update PatientSummary.js

--- DataSim/data-sim/src/components/patients/PatientSummary.js
+++ DataSim/data-sim/src/components/patients/PatientSummary.js
@@ -1,19 +1,66 @@
 import React from "react";
 
 const PatientSummary = ({patient}) => {
-    return (
-        <div className="card z-depth-0 project-summary light grey center">
-            <div className="card-content grey-text text-darken-3 hoverable">
-                <span className="card-title ">{patient.patientName}</span>
-                <p>Heart Rate - {patient.rHeartRate}</p>
-                <p>Body Temperature - {patient.bodyTempature}</p>
-                <p>Height - {patient.height}</p>
-                <p>Weight - {patient.weight}</p>
-                <p>Triage Tag - {patient.triageTag}</p>
-                <p className="grey-text">Active Nurse: {patient.activeNurse}</p>
+    if (patient.triageTag === 'Black') {
+        return (
+            <div className="card z-depth-0 project-summary light grey center">
+                <div className="card-content grey-text text-darken-3 hoverable">
+                    <span className="card-title ">{patient.patientName}</span>
+                    <p>Heart Rate - {patient.rHeartRate}</p>
+                    <p>Body Temperature - {patient.bodyTempature}</p>
+                    <p>Height - {patient.height}</p>
+                    <p>Weight - {patient.weight}</p>
+                    <p>Triage Tag - {patient.triageTag}</p>
+                    <p>Active Nurse: {patient.activeNurse}</p>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
+    else if (patient.triageTag === 'Blue') {
+        return (
+            <div className="card z-depth-0 project-summary light blue center">
+                <div className="card-content grey-text text-darken-3 hoverable">
+                    <span className="card-title ">{patient.patientName}</span>
+                    <p>Heart Rate - {patient.rHeartRate}</p>
+                    <p>Body Temperature - {patient.bodyTempature}</p>
+                    <p>Height - {patient.height}</p>
+                    <p>Weight - {patient.weight}</p>
+                    <p>Triage Tag - {patient.triageTag}</p>
+                    <p>Active Nurse: {patient.activeNurse}</p>
+                </div>
+            </div>
+        )
+    }
+    else if (patient.triageTag === 'Green') {
+        return (
+            <div className="card z-depth-0 project-summary light green center">
+                <div className="card-content grey-text text-darken-3 hoverable">
+                    <span className="card-title ">{patient.patientName}</span>
+                    <p>Heart Rate - {patient.rHeartRate}</p>
+                    <p>Body Temperature - {patient.bodyTempature}</p>
+                    <p>Height - {patient.height}</p>
+                    <p>Weight - {patient.weight}</p>
+                    <p>Triage Tag - {patient.triageTag}</p>
+                    <p>Active Nurse: {patient.activeNurse}</p>
+                </div>
+            </div>
+        )
+    }
+    else if (patient.triageTag === 'Red') {
+        return (
+            <div className="card z-depth-0 project-summary light red center">
+                <div className="card-content grey-text text-darken-3 hoverable">
+                    <span className="card-title ">{patient.patientName}</span>
+                    <p>Heart Rate - {patient.rHeartRate}</p>
+                    <p>Body Temperature - {patient.bodyTempature}</p>
+                    <p>Height - {patient.height}</p>
+                    <p>Weight - {patient.weight}</p>
+                    <p>Triage Tag - {patient.triageTag}</p>
+                    <p>Active Nurse: {patient.activeNurse}</p>
+                </div>
+            </div>
+        )
+    }
+};
 
 export default PatientSummary
