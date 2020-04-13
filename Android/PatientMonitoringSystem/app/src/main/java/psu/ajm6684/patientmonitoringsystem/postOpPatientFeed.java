1879c0276079be076f0dbb8cf78c61273ea34354
Alphonso Mckenzie <55410232+Alphonso74@users.noreply.github.com>
2020-04-13 15:54:36 -0400

    Boy Temp now on patient card for simulator display

--- Android/PatientMonitoringSystem/app/src/main/java/psu/ajm6684/patientmonitoringsystem/postOpPatientFeed.java
+++ Android/PatientMonitoringSystem/app/src/main/java/psu/ajm6684/patientmonitoringsystem/postOpPatientFeed.java
@@ -976,14 +976,14 @@ public class postOpPatientFeed extends AppCompatActivity {
                                 menuDialog.setMessage(patientName1 + " Is in critical condition!!!!");
                                 menuDialog.setCancelable(true);
 
-                                menuDialog.setPositiveButton("Button1", new DialogInterface.OnClickListener() {
+                                menuDialog.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
 
                                     }
                                 });
 
-                                menuDialog.setNegativeButton("Button2", new DialogInterface.OnClickListener() {
+                                menuDialog.setNegativeButton("Notify", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
 
