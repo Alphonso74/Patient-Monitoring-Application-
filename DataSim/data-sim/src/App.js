d2d36b6ab6f25de143d692a2dbbd19ddcca50666
11CTodd <59945448+11CTodd@users.noreply.github.com>
2020-04-13 20:01:18 -0400

    Update App.js

--- DataSim/data-sim/src/App.js
+++ DataSim/data-sim/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
               <Route path='/signin' component={SignIn} />
               <Route path='/signup' component={SignUp} />
               <Route path='/create' component={CreatePatient} />
-              <Route path='/patient/edit/:id' component={EditPatient} />
+              <Route path='/edit/:id' component={EditPatient} />
               <Route path='/chat' component={Chat} />
             </Switch>
           </div>
