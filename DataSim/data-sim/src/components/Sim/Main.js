d425d90c7f8400fc3480e91cb308af560f8aafc8
seantodd114 <57458555+seantodd114@users.noreply.github.com>
2020-04-27 13:46:14 -0400

    Simulator Routing Fix
    Logging out from the Simulator now goes to the Login page.

--- DataSim/data-sim/src/components/Sim/Main.js
+++ DataSim/data-sim/src/components/Sim/Main.js
@@ -4,10 +4,12 @@ import Feed from './Feed';
 import { connect } from 'react-redux';
 import { firestoreConnect} from "react-redux-firebase";
 import { compose } from "redux";
+import { Redirect } from 'react-router-dom';
 
 class Main extends Component {
     render(){
-        const { patients } = this.props;
+        const { patients, auth, user } = this.props;
+        if(!auth.uid) return <Redirect to='/signin'/>;
         return(
             <div className="App">
                 <div>
@@ -28,8 +30,13 @@ class Main extends Component {
 }
 
 const mapStateToProps = (state) => {
+    const id = state.firebase.auth.uid;
+    const users = state.firestore.data.Users;
+    const user = users ? users[id] : null;
     return {
-        patients: state.firestore.ordered.patients3
+        patients: state.firestore.ordered.patients3,
+        auth: state.firebase.auth,
+        user: user
     }
 };
 
