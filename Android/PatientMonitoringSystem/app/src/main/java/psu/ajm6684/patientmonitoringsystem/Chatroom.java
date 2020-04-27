4cb04b594877d7dc63c8b9693ec08b73af07437b
Alphonso74 <ajm6684@psu.edu>
2020-04-27 11:27:25 -0400

    color

--- Android/PatientMonitoringSystem/app/src/main/java/psu/ajm6684/patientmonitoringsystem/Chatroom.java
+++ Android/PatientMonitoringSystem/app/src/main/java/psu/ajm6684/patientmonitoringsystem/Chatroom.java
@@ -65,7 +65,7 @@ public class Chatroom extends AppCompatActivity {
         uid = user.getUid();
 
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
-        
+
         l1.setAdapter(adapter);
 
         l1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
